aeec45ce5cde709fc2c7cc4d7ba49d5ac0b5123a
dell ova kevin <kevin.dellova@hotmail.fr>
2022-11-20 12:07:24 +0100

    metadata exemple

--- src/services/generator.service.spec.ts
+++ src/services/generator.service.spec.ts
@@ -1,4 +1,4 @@
-import metadatas from "./metadata.json";
+import metadataExemple from "./metadata.exemple.json";
 import GeneratorService from "@services/generator.service";
 
 const generatorService = new GeneratorService();
@@ -6,38 +6,42 @@ const generatorService = new GeneratorService();
 describe("Test generator token", () => {
   it("generate value", async () => {
     for (let index = 0; index < 100; index++) {
-      const attributes = await generatorService.randomValue(metadatas.attributes.head);
+      const attributes = await generatorService.randomValue(
+        metadataExemple.attributes.head,
+      );
     }
   });
 
   it("generate attribute", async () => {
-    const attributes = await generatorService.generateAttribute(metadatas.attributes);
+    const attributes = await generatorService.generateAttribute(
+      metadataExemple.attributes,
+    );
   });
 
   it("generate metadata", async () => {
-    const attributes = await generatorService.generateMetadata(metadatas);
+    const attributes = await generatorService.generateMetadata(metadataExemple);
   });
 
   it("generate tokens", async () => {
     const quantity = 100;
-    const tokens = await generatorService.generateTokens(quantity, metadatas);
+    const tokens = await generatorService.generateTokens(quantity, metadataExemple);
     expect(tokens.length).toEqual(quantity);
     for (let index = 0; index < quantity; index++) {
-      expect(tokens[index].name).toEqual(metadatas.name);
-      expect(tokens[index].description).toEqual(metadatas.description);
-      expect(tokens[index].name).toEqual(metadatas.name);
+      expect(tokens[index].name).toEqual(metadataExemple.name);
+      expect(tokens[index].description).toEqual(metadataExemple.description);
+      expect(tokens[index].name).toEqual(metadataExemple.name);
       Object.keys(tokens[index].attributes).forEach(trait => {
-        expect(Object.keys(metadatas.attributes).includes(trait)).toBe(true);
+        expect(Object.keys(metadataExemple.attributes).includes(trait)).toBe(true);
       });
       expect(Object.keys(tokens[index].attributes).length).toEqual(
-        Object.keys(metadatas.attributes).length,
+        Object.keys(metadataExemple.attributes).length,
       );
     }
   });
 
   it("generate tokens And write them", async () => {
     const quantity = 100;
-    const tokens = await generatorService.generateTokens(quantity, metadatas);
+    const tokens = await generatorService.generateTokens(quantity, metadataExemple);
 
     await generatorService.writeJson(tokens);
 
